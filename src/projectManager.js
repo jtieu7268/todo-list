1693ace2529a28c279d6d10c18d375b13caf7997
jtieu7268 <77133493+jtieu7268@users.noreply.github.com>
2025-09-12 21:01:51 -0400

    Add module for projectManager with general structure

---
+++ src/projectManager.js
@@ -0,0 +1,13 @@
+export const projectManager = (function () {
+
+    let projectList = [];
+
+    const createProject = function () {}
+
+    const addProject = function () {}
+
+    const deleteProject = function () {}
+
+    return { createProject, addProject, deleteProject }
+
+})();
