fe6768746561eab7629c998976ffd66ceb3ae928
jtieu7268 <77133493+jtieu7268@users.noreply.github.com>
2025-09-12 21:19:16 -0400

    Implement basic functionalities of projectManager

--- src/projectManager.js
+++ src/projectManager.js
@@ -2,11 +2,26 @@ export const projectManager = (function () {
 
     let projectList = [];
 
-    const createProject = function () {}
+    const createProject = function (name) {
+        let id = Date.now().toString();
+        let taskList = [];
+        return { id, name, taskList };
+    }
 
-    const addProject = function () {}
+    const addProject = function (name) {
+        const project = createProject(name);
+        projectList.push(project);
+        return project.id;
+    }
 
-    const deleteProject = function () {}
+    const deleteProject = function (projectID) {
+        const projectIndex = getProjectIndex(projectID);
+        projectList.splice(projectID, 1);
+    }
+
+    const getProject = id => { return projectList.find(project => project.id === id) }
+
+    const getProjectIndex = id => { return projectList.findIndex(project => project.id === id) }
 
     return { createProject, addProject, deleteProject }
 
